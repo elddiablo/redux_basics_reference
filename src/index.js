bc8ec134dbd245a2b8a8aba1f67424e7e82f50c0
elddiablo <elddiablo001@gmail.com>
2019-01-16 22:43:04 +0200

    minor update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import { createStore, combineReducers } from 'redux';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/results';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore( reducer ); 
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+});
+const store = createStore( rootReducer ); 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
